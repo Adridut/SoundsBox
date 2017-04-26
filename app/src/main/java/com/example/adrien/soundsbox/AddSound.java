f17cba3df71ec0999e13c41f4d05b9bf4c685b93
adridut <dutfoy.adrien@hotmail.fr>
2017-04-26 20:34:25 +0200

    record on progress

--- app/src/main/java/com/example/adrien/soundsbox/AddSound.java
+++ app/src/main/java/com/example/adrien/soundsbox/AddSound.java
@@ -1,12 +1,21 @@
 package com.example.adrien.soundsbox;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.IOException;
 
 /**
  * Created by adrien on 03/03/17.
@@ -14,17 +23,42 @@ import android.widget.EditText;
 
 public class AddSound extends Activity {
 
-    Button save, back;
+    Button save, back, record, play;
+    String mFileName;
     EditText name;
+    boolean isRecordActive = false;
+    boolean isPlayActive = false;
+
+    private MediaRecorder mRecorder = null;
+    private MediaPlayer   mPlayer = null;
+
+    private static final String LOG_TAG = "AudioRecordTest";
+
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_sound_layout);
 
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
         name = (EditText) findViewById(R.id.sound_name);
         save = (Button) findViewById(R.id.save_button);
         back = (Button) findViewById(R.id.back_button);
+        record = (Button) findViewById(R.id.recordButton);
+        play = (Button) findViewById(R.id.play);
+
+
+        // Record to the external cache directory for visibility
+        mFileName = getExternalCacheDir().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +79,85 @@ public class AddSound extends Activity {
             }
         });
 
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRecordActive = !isRecordActive;
+                if (isRecordActive) {
+                    startRecording();
+                } else {
+                    stopRecording();
+                }
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPlayActive = !isPlayActive;
+                    startPlaying();
+            }
+        });
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
+
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
 }
